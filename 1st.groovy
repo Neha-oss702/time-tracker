6c9e16ba8c65f18b7f17562b1b872b97d582a219
Neha-oss702 <nbachchhere@gmail.com>
2023-02-17 16:52:32 +0530

    k55

--- 1st.groovy
+++ 1st.groovy
@@ -4,7 +4,7 @@ pipeline {
     stages {
         stage('Hello') {
             steps {
-                echo 'Hello Wmnmffmfvg'
+                echo 'Hello Wmnmffjijkhhgvgvbhb1111111mfvg'
             }
         }
     }
