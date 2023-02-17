b8115e07303796b96ad9b42284698b7ed7239010
Neha-oss702 <nbachchhere@gmail.com>
2023-02-17 16:16:00 +0530

    kl

--- 1st.groovy
+++ 1st.groovy
@@ -5,7 +5,7 @@ public class Tracker {
     private List<TimeEntry> entries;
 
     public void add(TimeEntry entry) {
-        entries.add(entry);
+        entries.remove(entry);
     }
 
     public void remove(TimeEntry entry) {
